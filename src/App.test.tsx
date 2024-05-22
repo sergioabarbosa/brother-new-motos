faedcbb5ef48a6550477b9fdefb0d3e29ecd9971
Sergio Alves Barbosa <sergio.alvesbarbosa@gmail.com>
2024-05-22 17:16:40 -0300

    add tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,68 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Verifica se a página renderiza o texto "Bem-vindo a Brother Motos!"', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const text = screen.getByText(/Bem-vindo a Brother Motos!/i);
+  expect(text).toBeInTheDocument();
 });
+
+test('Verifica se a página renderiza o texto "Confira nossas promoções e ofertas exclusivas!"', () => {
+  render(<App />);
+  const text = screen.getByText(/Confira nossas promoções e ofertas exclusivas!/i);
+  expect(text).toBeInTheDocument();
+});
+
+test('Verifica se a página renderiza o texto "Digite o que você procura"', () => {
+  render(<App />);
+  const placeholder = screen.getByPlaceholderText(/Buscar produtos.../i);
+  expect(placeholder).toBeInTheDocument();
+});
+
+test('Verifica se a página renderiza o botão "Buscar"', () => {
+  render(<App />);
+  const button = screen.getByRole('button', { name: /buscar/i });
+  expect(button).toBeInTheDocument();
+});
+
+test('Verifica se renderiaz um botão de busca', () => {
+  render(<App />);
+  const button = screen.getByRole('button', { name: /buscar/i });
+  expect(button).toBeInTheDocument();
+});
+
+test('Verifica se renderiza um input de texto', () => {
+  render(<App />);
+  const input = screen.getByRole('textbox');
+  expect(input).toBeInTheDocument();
+});
+
+test('Verifica se o input de texto está vazio inicialmente', () => {
+  render(<App />);
+  const input = screen.getByRole('textbox');
+  expect(input).toHaveValue('');
+});
+
+test('Verifica se o input de texto aceita digitação', () => {
+  render(<App />);
+  const input = screen.getByRole('textbox');
+  fireEvent.change(input, { target: { value: 'motos' } });
+  expect(input).toHaveValue('motos');
+});
+
+test('Verifica se o botão de busca está desabilitado quando o input de texto está vazio', () => {
+  render(<App />);
+  const button = screen.getByRole('button', { name: /buscar/i });
+  expect(button).toBeDisabled();
+});
+
+test('Verifica se o botão de busca está habilitado quando o input de texto está preenchido', () => {
+  render(<App />);
+  const input = screen.getByRole('textbox');
+  const button = screen.getByRole('button', { name: /buscar/i });
+  fireEvent.change(input, { target: { value: 'motos' } });
+  expect(button).toBeEnabled();
+});
+
+
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,122 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect, FormEvent } from 'react';
+import ReactPaginate from 'react-paginate';
+import { FaFacebook, FaTwitter, FaInstagram } from 'react-icons/fa';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface Product {
+  id: string;
+  title: string;
+  thumbnail: string;
+  price: number;
 }
 
-export default App;
+const ITEMS_PER_PAGE = 30;
+
+const Home: React.FC = () => {
+  const [query, setQuery] = useState<string>('');
+  const [results, setResults] = useState<Product[]>([]);
+  const [currentPage, setCurrentPage] = useState<number>(0);
+  const [totalPages, setTotalPages] = useState<number>(0);
+  const [currentQuery, setCurrentQuery] = useState<string>('');
+
+  useEffect(() => {
+    const fetchInitialProducts = async () => {
+      try {
+        const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=motocicleta`);
+        const data = await response.json();
+        const initialProducts = data.results.slice(0, ITEMS_PER_PAGE);
+        setResults(initialProducts);
+        const totalResults = data.results.length;
+        const pages = Math.ceil(totalResults / ITEMS_PER_PAGE);
+        setTotalPages(pages);
+      } catch (error) {
+        console.error('Error fetching initial products:', error);
+      }
+    };
+
+    fetchInitialProducts();
+  }, []);
+
+  const handleSearch = async (event: FormEvent) => {
+    event.preventDefault();
+    try {
+      const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${query}`);
+      const data = await response.json();
+      const searchResults = data.results.slice(0, ITEMS_PER_PAGE);
+      setResults(searchResults);
+      const totalResults = data.results.length;
+      const pages = Math.ceil(totalResults / ITEMS_PER_PAGE);
+      setTotalPages(pages);
+      setCurrentPage(0);
+      setCurrentQuery(query);
+    } catch (error) {
+      console.error('Error fetching data from Mercado Livre API:', error);
+    }
+  };
+
+  const handlePageChange = async (selectedItem: { selected: number }) => {
+    const offset = selectedItem.selected * ITEMS_PER_PAGE;
+    setCurrentPage(selectedItem.selected);
+    try {
+      const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${currentQuery}&offset=${offset}`);
+      const data = await response.json();
+      const pageResults = data.results.slice(0, ITEMS_PER_PAGE);
+      setResults(pageResults);
+    } catch (error) {
+      console.error('Error fetching data from Mercado Livre API:', error);
+    }
+  };
+
+  return (
+    <div id="root">
+      <header className="banner">
+        <h1>Bem-vindo a Brother Motos!</h1>
+        <p>Confira nossas promoções e ofertas exclusivas!</p>
+        <div className="social-icons">
+          <FaFacebook />
+          <FaTwitter />
+          <FaInstagram />
+        </div>
+      </header>
+      <h1 className="title">Busca no Mercado Livre</h1>
+      <form onSubmit={handleSearch} className="form">
+        <input
+          type="text"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          placeholder="Buscar produtos..."
+          className="input"
+        />
+        <button type="submit" className="button" disabled={!query.trim().length}>Buscar</button>
+      </form>
+      <div className="card-container">
+        {results.map((item) => (
+          <div key={item.id} className="card">
+            <h2 className="productTitle">{item.title}</h2>
+            <img src={item.thumbnail} alt={item.title} className="image" />
+            <p className="productPrice">Preço: R$ {item.price}</p>
+          </div>
+        ))}
+      </div>
+      {query && totalPages > 1 && (
+        <ReactPaginate
+          previousLabel={'Anterior'}
+          nextLabel={'Próxima'}
+          breakLabel={'...'}
+          pageCount={totalPages}
+          marginPagesDisplayed={2}
+          pageRangeDisplayed={5}
+          onPageChange={handlePageChange}
+          containerClassName="pagination"
+          activeClassName="active"
+          forcePage={currentPage}
+        />
+      )}
+      <footer className="footer">
+        &copy; {new Date().getFullYear()} Brother Motos. Todos os direitos reservados.
+      </footer>
+    </div>
+  );
+};
+
+export default Home;
